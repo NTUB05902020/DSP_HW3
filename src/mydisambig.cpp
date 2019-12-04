9c187bc6ced08a944f08fb98ddf5449b97925372
NTUB05902020 <b05902020@ntu.edu.tw>
2019-12-04 22:57:39 +0800

    new

--- src/mydisambig.cpp
+++ src/mydisambig.cpp
@@ -102,7 +102,7 @@ void getMap(const char *map_path, const int map_order){
     for(int i=0;i<5;++i,++it){
         it->first.print();  printf(":  ");
         for(int j=0;j<(it->second.size());++j){
-            it->second.print();  printf(" ");
+            it->second[j].print();  printf(" ");
         }
         printf("\n");
     }
